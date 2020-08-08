f46414d01512563d95ed3edce83c0c6d414800a8
estudiante <estudiante@ISIS4426-24.virtual.uniandes.edu.co>
2020-08-08 11:02:43 -0500

    settings and api created

--- events/settings.py
+++ events/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0ih=@69szw6gm%jli(dnls0e@%jst#ubgprbc0fpm-al9q(icn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'api'
 ]
 
 MIDDLEWARE = [
