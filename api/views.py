d580faa37274842fdcfbe7cbea60002834c198c3
Magda Mejía <magda.mejia96@gmail.com>
2020-08-09 09:51:17 -0500

    date filter

--- api/views.py
+++ api/views.py
@@ -5,7 +5,6 @@ from rest_framework import viewsets
 from .models import Event
 from .serializers import EventSerializer
 
-
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all().order_by('-created_at')
     serializer_class = EventSerializer
